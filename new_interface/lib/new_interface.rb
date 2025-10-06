806fcd0121a7c47cb23feff7e30837e011196fc6
vladimir kosyakin <vovakos@bk.ru>
2025-10-06 14:28:04 +0300

    удалил лишнее

--- new_interface/lib/new_interface.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'redmine'
-
-require_relative 'new_interface/menu'
-
-Rails.configuration.to_prepare do
-	# no-op
-end
--- new_interface/lib/new_interface/hooks.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module NewInterface
-	class Hooks < Redmine::Hook::ViewListener
-		# Insert scripts into the <head> for all pages
-		render_on :view_layouts_base_html_head, partial: 'new_interface/assets'
-	end
-end
-
